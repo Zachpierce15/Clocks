f0c9b1142819b43273bfe1d674cc33554850659c
Zachary <zachary.15pierce@gmail.com>
2019-07-18 13:32:08 -0600

    Css now working

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
 import App from './components/app.jsx'
+import '../../stylesheet.css'
 
 const title = 'React with Webpack and Babel';
 
--- webpack.config.js
+++ webpack.config.js
@@ -6,11 +6,15 @@ module.exports = {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: ['babel-loader']
+      },
+      {
+        test: /\.css$/,
+        use:['style-loader','css-loader']
       }
     ]
   },
   resolve: {
-    extensions: ['*', '.js', '.jsx']
+    extensions: ['*', '.js', '.jsx', 'css']
   },
   output: {
     path: __dirname + '/dist',
