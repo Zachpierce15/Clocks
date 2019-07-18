7facb2189ab372c08d4050f33019b3c1b359484d
Zachary <zachary.15pierce@gmail.com>
2019-07-18 13:44:22 -0600

    Clock is now keeping track of current time

--- client/src/components/app.jsx
+++ client/src/components/app.jsx
@@ -1,15 +1,24 @@
 import React from 'react';
+import { setInterval } from 'timers';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {time: new Date()};
     //bind functions here
+  }
+  currentTime() {
+    this.setState({
+      time: new Date()
+    })
+  }
+  componentDidMount() {
+    setInterval(() => this.currentTime(), 1000)
   }
   render() {
     return (
     <div className='practice'>
-      CSS Loaders are awesome! 
+      {this.state.time.toLocaleTimeString()}
     </div>
     )
   }
